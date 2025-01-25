8d857854eb9e2afb760ea310f717945323e3eeb6
sachin kumar <goputen215@gmail.com>
2025-01-25 13:02:33 +0530

    add user agend navigator

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ let container = document.getElementById("container");
 let sortButton = document.getElementById("sort");
 let arr = [];
 let timeout;
+let total = (navigator.userAgent.match(/Mobi/) ? 15 : 30);
 
 random();
 function random() {
-  clearTimeout(timeout)
+  clearTimeout(timeout);
   sortButton.disabled = false;
   container.innerHTML = "";
 
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < total; i++) {
     arr[i] = Math.floor(Math.random() * 95) + 1;
     show();
   }
@@ -23,13 +24,13 @@ function sort() {
 }
 
 function play(swaps) {
-  if (swaps.lenth == 0) {
+  if (swaps.length == 0) {
     return;
   }
   const [i, j] = swaps.shift();
   [arr[i], arr[j]] = [arr[j], arr[i]];
   show();
-  timeout=setTimeout(() => {
+  timeout = setTimeout(() => {
     play(swaps);
   }, 130);
 }
@@ -37,8 +38,8 @@ function play(swaps) {
 function bubbleSort(arr) {
   const swaps = [];
   let temp;
-  for (let i = 0; i < 30; i++) {
-    for (let j = 0; j < 30; j++) {
+  for (let i = 0; i < total; i++) {
+    for (let j = 0; j < total; j++) {
       if (arr[j] > arr[j + 1]) {
         temp = arr[j];
         arr[j] = arr[j + 1];
@@ -52,7 +53,7 @@ function bubbleSort(arr) {
 
 function show() {
   container.innerHTML = "";
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < total; i++) {
     let div = document.createElement("div");
     div.style.height = `${arr[i]}%`;
     div.classList.add("bar");
